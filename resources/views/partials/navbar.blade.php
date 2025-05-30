ac63cbde585a4becc51c7d58461b875ccd7d1c2f
unknown <raflinailurrizqi@gmail.com>
2025-05-31 00:15:16 +0700

    main & footer

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <nav>
+        <section>
+            <h4></h4>
+            <div class="">
+                <ul class="nav-list">
+                    <li class="nav-item">?</a></li>
+                    <li class="nav-item">Employers</a></li>
+                    <li class="nav-item">Sign Up</a></li>
+                </ul>
+            </div>
+        </section>
+        <section>
+            <ul>
+                <li>Jobs</li>
+                <li>Scholarship</li>
+                <li>Create CV With AI</li>
+            </ul>
+        </section>
+    </nav>
+</body>
+</html>
