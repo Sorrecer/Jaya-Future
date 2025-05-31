5f401e5a4dd9a956721fb4465a59a973e5c9d20d
Kenny Anaga <32734825+Sorrecer@users.noreply.github.com>
2025-06-01 00:54:53 +0700

    conflict fix + homepage

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,16 +6,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- logo web -->
-    <link rel="shortcut icon" href="{{asset('logo/logo2.png')}}">
-
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/ken-style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/yap-style.css') }}">
+
 
     <!-- Bootstraps Icon -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    
+
+    <!-- {{-- font --}} -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+
+
     <title>Plerr</title>
 </head>
 
@@ -26,7 +31,21 @@
 
 
 
+    {{-- Navbar --}}
     @include('partials.navbar')
+
+    {{-- Hero --}}
+
+    {{-- Konten kosong --}}
+    <div class="main-content">
+        {{-- Bisa ditambah konten di sini --}}
+
+        @yield('container')
+
+    </div>
+
+    {{-- Footer --}}
+    @include('components.footer')
 
 
 
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    
-    <nav>
-        <section>
-            <h4></h4>
-            <div class="">
-                <ul class="nav-list">
-                    <li class="nav-item">?</a></li>
-                    <li class="nav-item">Employers</a></li>
-                    <li class="nav-item">Sign Up</a></li>
-                </ul>
-            </div>
-        </section>
-        <section>
-            <ul>
-                <li>Jobs</li>
-                <li>Scholarship</li>
-                <li>Create CV With AI</li>
-            </ul>
-        </section>
-    </nav>
-</body>
-</html>
-=======
-
 <nav>
     <section class="container d-flex justify-content-between align-items-center py-3">
         <h4><a href="#" class="text-decoration-none">Jayafuture</a></h4>
@@ -47,4 +13,3 @@
         <div class="px-4">Create CV With AI</div>
     </section>
 </nav>
->>>>>>> b503214 (Membaut halaman home)
