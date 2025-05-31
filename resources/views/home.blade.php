78a21442aeac20aa760884f2374efdaeaa751ad3
unknown <raflinailurrizqi@gmail.com>
2025-06-01 00:10:45 +0700

    homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,85 @@
+@extends('layout.main')
+
+@section('container')
+<main>
+
+    {{-- search bar --}}
+    <section class="container">
+        <h3 class="text-center my-5">Start Your Jaya Future Carrier</h3>
+        <div class="search">
+            <form action="" method="get" id="search-form">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Junior / Intern Position">
+                    <button class="btn btn-outline-secondary" type="submit" id="button">Cari</button>
+                </div>
+            </form>
+        </div>
+        <p class="text-center">Employers?<a href="#">Post your job here</a></p>
+        <div class="job-category d-flex flex-wrap justify-content-center gap-3 my-5">
+            <div class="p-2 border rounded">Easy Apply</div>
+            <div class="p-2 border rounded">Part Time</div>
+            <div class="p-2 border rounded">Customer Service</div>
+            <div class="p-2 border rounded">Data Analyst</div>
+        </div>
+    </section>
+
+    {{-- job Offers --}}
+    <section class="border-top my-5">
+        <h3 class="text-center my-5">Exclusive Offers</h3>
+        <div class="job container d-flex flex-wrap justify-content-center gap-3 mb-5">
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <img src="{{ asset('img/halal.jpg') }}" alt="logo" class="rounded-circle mb-3" style="width: 50px; height: 50px;">
+                    <h5 class="card-title">Product Design</h5>
+                    <p class="card-text">Porto, Portugal <strong>(On site)</strong></p>
+                    <div class="d-flex">
+                        <a href="#" class="text-decoration-none border rounded-pill p-1 me-1">Easy Apply</a>
+                        <a href="#" class="text-decoration-none border rounded-pill p-1 me-1">Multiple Candidate</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <img src="{{ asset('img/halal.jpg') }}" alt="logo" class="rounded-circle mb-3" style="width: 50px; height: 50px;">
+                    <h5 class="card-title">Product Design</h5>
+                    <p class="card-text">Porto, Portugal <strong>(On site)</strong></p>
+                    <div class="d-flex">
+                        <a href="#" class="text-decoration-none border rounded-pill p-1 me-1">Easy Apply</a>
+                        <a href="#" class="text-decoration-none border rounded-pill p-1 me-1">Multiple Candidate</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <img src="{{ asset('img/halal.jpg') }}" alt="logo" class="rounded-circle mb-3" style="width: 50px; height: 50px;">
+                    <h5 class="card-title">Product Design</h5>
+                    <p class="card-text">Porto, Portugal <strong>(On site)</strong></p>
+                    <div class="d-flex">
+                        <a href="#" class="text-decoration-none border rounded-pill p-1 me-1">Easy Apply</a>
+                        <a href="#" class="text-decoration-none border rounded-pill p-1 me-1">Multiple Candidate</a>
+                    </div>
+                </div>
+            </div>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <img src="{{ asset('img/halal.jpg') }}" alt="logo" class="rounded-circle mb-3" style="width: 50px; height: 50px;">
+                    <h5 class="card-title">Product Design</h5>
+                    <p class="card-text">Porto, Portugal <strong>(On site)</strong></p>
+                    <div class="d-flex">
+                        <a href="#" class="text-decoration-none border rounded-pill p-1 me-1">Easy Apply</a>
+                        <a href="#" class="text-decoration-none border rounded-pill p-1 me-1">Multiple Candidate</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="hero my-5">
+        <h3 class="text-center my-5">Have You Graduated Or Changed Your Career?</h3>
+        <div class="container d-flex justify-content-center gap-5">
+            
+        </div>
+    </section>
+
+</main>
+@endsection
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,3 +30,21 @@
     </nav>
 </body>
 </html>
+=======
+
+<nav>
+    <section class="container d-flex justify-content-between align-items-center py-3">
+        <h4><a href="#" class="text-decoration-none">Jayafuture</a></h4>
+        <div class="d-flex align-items-center">
+            <div class="px-3 border-end"><i class="bi bi-question-circle"></i></div>
+            <div class="px-3 border-end">Employers</div>
+            <div class=" btn btn-dark rounded-sm px-3"><i class="bi bi-box-arrow-in-right"></i> Sign Up</div>
+        </div>
+    </section>
+    <section class="nav d-flex justify-content-center py-3 border">
+        <div class="px-4">Jobs</div>
+        <div class="px-4">Scholarship</div>
+        <div class="px-4">Create CV With AI</div>
+    </section>
+</nav>
+>>>>>>> b503214 (Membaut halaman home)
