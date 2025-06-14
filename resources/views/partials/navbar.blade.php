8491b06ab781407c03a1ccffc213362a9236267d
Kenny Anaga <32734825+Sorrecer@users.noreply.github.com>
2025-06-15 00:55:25 +0700

    lil fix on navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -5,33 +5,33 @@
             <div class="pe-3 border-end"><i class="bi bi-question-circle"></i></div>
 
             @guest
-                <a href="{{ route('login') }}">
-                    <div class=" btn  pe-3 border-end"> Sign In</div>
-                </a>
-                <a href="{{ route('register') }}">
-                    <div class=" btn btn-dark rounded-pill"><i class="bi bi-box-arrow-in-right"></i> Sign Up</div>
-                </a>
+            <a href="{{ route('login') }}">
+                <div class=" btn  pe-3 border-end"> Sign In</div>
+            </a>
+            <a href="{{ route('register') }}">
+                <div class=" btn btn-dark rounded-pill"><i class="bi bi-box-arrow-in-right"></i> Sign Up</div>
+            </a>
             @endguest
 
             @auth
-                <div class="pe-3 ">Halo, {{ Auth::user()->name }}</div>
-                <div class="dropdown">
-                    <div class="rounded-circle overflow-hidden border dropdown" data-bs-toggle="dropdown"
-                        aria-expanded="false" style="width: 40px; height: 40px;">
-                        <img src="{{ Storage::url(Auth::user()->profile_picture) }}" alt="">
-                    </div>
-                    <ul class="dropdown-menu">
-                        <li><a href="{{ route('profile') }}" class="dropdown-item p-2" > My Profil</a></li>
-                        <li><button class="dropdown-item p-2" type="button"> Application</button></li>
-                        <li>
-                            <form action="{{ route('logout') }}" method="post">
-                                @csrf
-                                <button type="submit" class="dropdown-item">Sign Out</button>
-                            </form>
-                        </li>
-
-                    </ul>
+            <div class="pe-3 ">Halo, {{ Auth::user()->name }}</div>
+            <div class="dropdown">
+                <div class="rounded-circle overflow-hidden border dropdown" data-bs-toggle="dropdown" aria-expanded="false" style="width: 40px; height: 40px;">
+                    <img src="{{ asset('img/logo.jpg') }}" alt="" style="width: 100%; height: 100%; object-fit: cover;">
                 </div>
+
+                <ul class="dropdown-menu">
+                    <li><a href="{{ route('profile') }}" class="dropdown-item p-2"> My Profil</a></li>
+                    <li><button class="dropdown-item p-2" type="button"> Application</button></li>
+                    <li>
+                        <form action="{{ route('logout') }}" method="post">
+                            @csrf
+                            <button type="submit" class="dropdown-item p-2">Sign Out</button>
+                        </form>
+                    </li>
+
+                </ul>
+            </div>
             @endauth
 
         </div>
